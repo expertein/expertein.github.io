daa05126e384e852a7ae318fb622c005190a24dd
expertein <expertein@gmail.com>
2018-04-20 16:27:27 +0800

    update url

--- mango/demo.php
+++ mango/demo.php
@@ -1,5 +1,5 @@
-<NAME>Movies</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
-<NAME>TV Show</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
-<NAME>Music</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
-<NAME>Search</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
-<NAME>Favourites</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Movies</NAME><URL>https://expertein.github.io/mango/movies.php</URL><ICON>https://expertein.github.io/mango/movie.png</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>TV Show</NAME><URL>https://expertein.github.io/mango/tvshow.php</URL><ICON>https://expertein.github.io/mango/tvshow.png</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Music</NAME><URL>https://expertein.github.io/mango/music.php</URL><ICON>https://expertein.github.io/mango/music.png</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Search</NAME><URL> </URL><ICON>https://expertein.github.io/mango/search.png</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Favourites</NAME><URL> </URL><ICON>https://expertein.github.io/mango/favourite.png</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
