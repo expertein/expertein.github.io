4d77ed948bfc28eae16d5331f4b19a4c1c496223
expertein <expertein@gmail.com>
2018-04-20 16:12:10 +0800

    change file

---
+++ mango/demo.php
@@ -0,0 +1,5 @@
+<NAME>Movies</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>TV Show</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Music</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Search</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
+<NAME>Favourites</NAME><URL>(.+?)</URL><ICON>(.+?)</ICON><FANART>(.+?)</FANART><DESC>(.+?)</DESC>
